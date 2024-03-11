cae001e09b05b6688e59334dc1039de3053e7261
Arnaldo Junior <abs.junnior@hotmail.com>
2024-03-11 16:45:28 -0300

    Refatorando e criando novo componente

---
+++ lib/components/product_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/model/product.dart';
+import 'package:shop/model/product_list.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadProducts = provider.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadProducts.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, index) {
+        return ProductItem(product: loadProducts[index]);
+      },
+    );
+  }
+}
