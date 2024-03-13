71d25848edd9ca644ccae6edf184e2570d6fa9b0
Arnaldo Junior <abs.junnior@hotmail.com>
2024-03-12 23:03:55 -0300

    Aplicando varios Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/model/cart.dart';
 import 'package:shop/model/product_list.dart';
 // import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/product_datail_page.dart';
@@ -17,25 +18,35 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //    return CounterProvider(
 
-    return MaterialApp(
-      title: "Meu Shop",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        primarySwatch: Colors.purple,
-        hintColor: Colors.deepOrange,
-        appBarTheme: const AppBarTheme(
-          color: Colors.purple,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ],
+      child: MaterialApp(
+        title: "Meu Shop",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          primarySwatch: Colors.purple,
+          hintColor: Colors.deepOrange,
+          appBarTheme: const AppBarTheme(
+            color: Colors.purple,
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
           ),
         ),
+        home: const ProductsOverviewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (context) => const ProductDatailPage(),
+        },
       ),
-      home: const ProductsOverviewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (context) => const ProductDatailPage(),
-      },
     );
   }
 }
