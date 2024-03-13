c1f98d42e89e87e33dd1abb86573a6c3f477bc42
Arnaldo Junior <abs.junnior@hotmail.com>
2024-03-13 17:36:30 -0300

    Personalização do widget para exibir informações do produto

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -18,7 +18,7 @@ class CartPage extends StatelessWidget {
       body: Column(
         children: [
           Card(
-            margin: const EdgeInsets.all(20),
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
