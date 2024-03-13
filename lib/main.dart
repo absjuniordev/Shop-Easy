164be3d9c0bffb5ec03d7010ae2173c71a754a3b
Arnaldo Junior <abs.junnior@hotmail.com>
2024-03-12 22:52:43 -0300

    Adição do metodo do total do carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/model/product_list.dart';
 // import 'package:shop/pages/counter_page.dart';
-import 'package:flutter/material.dart';
 import 'package:shop/pages/product_datail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -16,28 +17,25 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //    return CounterProvider(
 
-    return ChangeNotifierProvider(
-      create: (_) => ProductList(),
-      child: MaterialApp(
-        title: "Meu Shop",
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Lato',
-          primarySwatch: Colors.purple,
-          hintColor: Colors.deepOrange,
-          appBarTheme: const AppBarTheme(
-            color: Colors.purple,
-            titleTextStyle: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-            ),
+    return MaterialApp(
+      title: "Meu Shop",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        primarySwatch: Colors.purple,
+        hintColor: Colors.deepOrange,
+        appBarTheme: const AppBarTheme(
+          color: Colors.purple,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
           ),
         ),
-        home: const ProductsOverviewPage(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (context) => const ProductDatailPage(),
-        },
       ),
+      home: const ProductsOverviewPage(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (context) => const ProductDatailPage(),
+      },
     );
   }
 }
