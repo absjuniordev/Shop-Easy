0e15cabf69d2f529f31651f8f46633896fee1a33
Arnaldo Junior <abs.junnior@hotmail.com>
2024-03-14 17:16:46 -0300

    Exibindo as caracteristicas dos itens

--- lib/pages/product_datail_page.dart
+++ lib/pages/product_datail_page.dart
@@ -12,7 +12,45 @@ class ProductDatailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.nome),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'R\$ ${product.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                textAlign: TextAlign.center,
+                product.description,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
