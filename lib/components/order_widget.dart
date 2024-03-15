9af651a7a6777ee2e1576505e9581af7ab52e147
Arnaldo Junior <abs.junnior@hotmail.com>
2024-03-14 21:40:16 -0300

    Complementação das informações dos detalhes do pedido

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -2,24 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:shop/model/order.dart';
 import 'package:intl/intl.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
   const OrderWidget({super.key, required this.order});
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text(
-          'R\$ ${order.total.toStringAsFixed(2)}',
-        ),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
-        ),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.expand_more),
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              'R\$ ${widget.order.total.toStringAsFixed(2)}',
+            ),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: const Icon(Icons.expand_more),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              height: (widget.order.products.length * 28.0) + 10,
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              child: ListView(
+                children: widget.order.products.map(
+                  (products) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          products.nome,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '${products.quantity} x R\$ ${products.price}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ).toList(),
+              ),
+            )
+        ],
       ),
     );
   }
