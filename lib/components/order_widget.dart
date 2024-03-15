3efc213a915c2a8beb20189d51acdc64c46666f8
Arnaldo Junior <abs.junnior@hotmail.com>
2024-03-14 21:24:06 -0300

    Criação de um Widget para apresentação dos detalhes dos pedidos

---
+++ lib/components/order_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shop/model/order.dart';
+import 'package:intl/intl.dart';
+
+class OrderWidget extends StatelessWidget {
+  final Order order;
+  const OrderWidget({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          'R\$ ${order.total.toStringAsFixed(2)}',
+        ),
+        subtitle: Text(
+          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.expand_more),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/order_widget.dart';
+import 'package:shop/model/order_list.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final orderList = Provider.of<OrderList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus pedidos'),
+        centerTitle: true,
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: orderList.itemsCounts,
+        itemBuilder: (ctx, i) {
+          return OrderWidget(
+            order: orderList.items[i],
+          );
+        },
+      ),
+      drawer: const AppDrawer(),
     );
   }
 }
