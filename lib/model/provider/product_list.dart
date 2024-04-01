98b92ed56f5e27406936d42a765d38fb15832f83
Arnaldo Junior <abs.junnior@hotmail.com>
2024-04-01 16:29:01 -0300

    Atualizando produto no BD

--- lib/model/provider/product_list.dart
+++ lib/model/provider/product_list.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:shop/model/provider/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _url =
-      "https://shop-app-3837f-default-rtdb.firebaseio.com/products.json";
+  final _baseUrl =
+      "https://shop-app-3837f-default-rtdb.firebaseio.com/products";
   final List<Product> _items = [];
 
   List<Product> get items => [..._items];
@@ -32,19 +32,29 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
+      await http.patch(
+        Uri.parse("$_baseUrl/${product.id}.json"),
+        body: jsonEncode(
+          {
+            'name': product.name,
+            'description': product.description,
+            'price': product.price,
+            'imageUrl': product.imageUrl,
+          },
+        ),
+      );
       _items[index] = product;
       notifyListeners();
     }
-    return Future.value();
   }
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse("$_baseUrl.json"));
 
     if (response.body == 'null') return; //caso BD vazio
 
@@ -70,7 +80,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse("$_baseUrl.json"),
       body: jsonEncode(
         {
           'name': product.name,
