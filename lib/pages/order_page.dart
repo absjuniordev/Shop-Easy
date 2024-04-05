edb155e76a0a1e710f29300d53f69ed9a83e83eb
Arnaldo Junior <abs.junnior@hotmail.com>
2024-04-04 22:19:42 -0300

    Refatorando codigo - OrderPage

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -4,48 +4,24 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_widget.dart';
 import 'package:shop/model/provider/order_list.dart';
 
-class OrderPage extends StatefulWidget {
+class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
 
   @override
-  State<OrderPage> createState() => _OrderPageState();
-}
-
-class _OrderPageState extends State<OrderPage> {
-  bool _isLoading = true;
-
-  Future<void> _refreshProducts(BuildContext context) {
-    return Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((_) {
-      setState(() => _isLoading = false);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderList = Provider.of<OrderList>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus pedidos'),
         centerTitle: true,
       ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: () => _refreshProducts(context),
-              child: ListView.builder(
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orderList, child) => ListView.builder(
                 itemCount: orderList.itemsCounts,
                 itemBuilder: (ctx, i) {
                   return OrderWidget(
@@ -53,7 +29,10 @@ class _OrderPageState extends State<OrderPage> {
                   );
                 },
               ),
-            ),
+            );
+          }
+        },
+      ),
       drawer: const AppDrawer(),
     );
   }
